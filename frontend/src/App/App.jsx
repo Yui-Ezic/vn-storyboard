86fdefffadd84eb2fa6a4438ba5ca0a58ff7ffdb
Yui-Ezic <mishan221199@gmail.com>
2021-05-01 21:11:08 +0300

    Added routing

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -1,69 +1,22 @@
 import React from 'react';
 import './App.css';
-import {Layer, Rect, Stage} from "react-konva";
-
-function generateShapes() {
-    return [...Array(1)].map((_, i) => ({
-        id: i.toString(),
-        x: Math.random() * window.innerWidth,
-        y: Math.random() * window.innerHeight,
-        isDragging: false,
-    }));
-}
-
-const INITIAL_STATE = generateShapes();
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import ProjectList from "../Project/List";
 
 function App() {
-    const [rects, setRects] = React.useState(INITIAL_STATE);
-
-    const handleDragStart = (e) => {
-        const id = e.target.id();
-        console.log(rects);
-        setRects(
-            rects.map((rect) => {
-                return {
-                    ...rect,
-                    isDragging: rect.id === id,
-                };
-            })
-        );
-    };
-    const handleDragEnd = (e) => {
-        setRects(
-            rects.map((rect) => {
-                return {
-                    ...rect,
-                    isDragging: false,
-                };
-            })
-        );
-    };
-
     return (
-        <Stage width={window.innerWidth} height={window.innerHeight}>
-            <Layer>
-                {rects.map((rect) => (
-                    <Rect
-                        key={rect.id}
-                        x={rect.x}
-                        y={rect.y}
-                        width={100}
-                        height={100}
-                        fill="red"
-                        shadowColor="black"
-                        shadowBlur={10}
-                        shadowOpacity={0.6}
-                        shadowOffsetX={rect.isDragging ? 10 : 5}
-                        shadowOffsetY={rect.isDragging ? 10 : 5}
-                        scaleX={rect.isDragging ? 1.2 : 1}
-                        scaleY={rect.isDragging ? 1.2 : 1}
-                        draggable
-                        onDragStart={handleDragStart}
-                        onDragEnd={handleDragEnd}
-                    />
-                ))}
-            </Layer>
-        </Stage>
+        <BrowserRouter>
+            <div className="app">
+                <Switch>
+                    <Route exact path="/">
+                        <ProjectList/>
+                    </Route>
+                    <Route exact path="/hello">
+                        <h1>Hello, world!</h1>
+                    </Route>
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 }
 
---
+++ frontend/src/Project/List/ProjectList.jsx
@@ -0,0 +1,12 @@
+function ProjectList() {
+    return (
+        <div>
+            <h1>Projects: </h1>
+            <ul>
+                <li>Add new project</li>
+            </ul>
+        </div>
+    )
+}
+
+export default ProjectList;
